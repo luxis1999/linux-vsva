e6f9eb35d4d5e8f6cf0806ae10f5f6caea43479e
Jacob Pan <jacob.jun.pan@linux.intel.com>
2020-03-18 15:55:51 -0700

    iommu/ioasid: Introduce per set allocation APIs
    IOASID set defines a group of IDs that share the same token. The
    ioasid_set concept helps to do permission checking among users as in the
    current code.
    
    With guest SVA usage, each VM has its own IOASID set. More
    functionalities are needed:
    1. Enforce quota, each guest may be assigned limited quota such that one
    guest cannot abuse all the system resource.
    2. Stores IOASID mapping between guest and host IOASIDs
    3. Per set operations, e.g. free the entire set
    
    For each ioasid_set token, a unique set ID is assigned. This makes
    reference of the set and data lookup much easier to implement.
    
    Signed-off-by: Liu Yi L <yi.l.liu@intel.com>
    Signed-off-by: Jacob Pan <jacob.jun.pan@linux.intel.com>

--- include/linux/ioasid.h
+++ include/linux/ioasid.h
@@ -41,6 +41,9 @@ int ioasid_register_allocator(struct ioasid_allocator_ops *allocator);
 void ioasid_unregister_allocator(struct ioasid_allocator_ops *allocator);
 int ioasid_set_data(ioasid_t ioasid, void *data);
 void ioasid_install_capacity(ioasid_t total);
+int ioasid_alloc_set(struct ioasid_set *token, ioasid_t quota, int *sid);
+void ioasid_free_set(int sid, bool destroy_set);
+int ioasid_find_sid(ioasid_t ioasid);
 #else /* !CONFIG_IOASID */
 static inline ioasid_t ioasid_alloc(struct ioasid_set *set, ioasid_t min,
 				    ioasid_t max, void *private)
@@ -49,6 +52,15 @@ static inline ioasid_t ioasid_alloc(struct ioasid_set *set, ioasid_t min,
 }
 
 static inline void ioasid_free(ioasid_t ioasid)
+{
+}
+
+static inline int ioasid_alloc_set(struct ioasid_set *token, ioasid_t quota, int *sid)
+{
+	return -ENOTSUPP;
+}
+
+static inline void ioasid_free_set(int sid, bool destroy_set)
 {
 }
 
@@ -75,5 +87,6 @@ static inline int ioasid_set_data(ioasid_t ioasid, void *data)
 static inline void ioasid_install_capacity(ioasid_t total)
 {
 }
+
 #endif /* CONFIG_IOASID */
 #endif /* __LINUX_IOASID_H */
